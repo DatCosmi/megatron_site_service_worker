e9100b15835257973f78b84be8d2326eccb51bb9
datcosmi <ivan03idk@gmail.com>
2024-11-05 15:48:34 -0600

    mas cambios al header jaja

--- app/components/header.js
+++ app/components/header.js
@@ -9,16 +9,20 @@ const Header = () => {
   const pathname = usePathname();
 
   useEffect(() => {
+    const isProductDetailPage =
+      pathname.startsWith("/productos/") && pathname.length > 11;
+
     const handleScroll = () => {
-      const isScrolled = window.scrollY > 20;
+      const isScrolled = window.scrollY > 20 || isProductDetailPage;
       setScrolled(isScrolled);
     };
 
     window.addEventListener("scroll", handleScroll);
-    return () => window.removeEventListener("scroll", handleScroll);
-  }, []);
+    handleScroll(); // Llama inmediatamente para establecer el estado si ya estás en `/productos/[id]`
 
-  // Función auxiliar para verificar si una ruta está activa
+    return () => window.removeEventListener("scroll", handleScroll);
+  }, [pathname]);
+
   const isRouteActive = (route) => {
     if (route === "/") {
       return pathname === "/";
@@ -29,30 +33,29 @@ const Header = () => {
   return (
     <header
       className={`
-        fixed top-0 w-full bg-white z-50 transition-all duration-300 ease-in-out
-        ${scrolled ? "h-14 border-b border-gray-200" : "h-16 "}
+        fixed top-0 w-full z-50 transition-all duration-300 ease-in-out
+        ${scrolled ? "h-14 border-b border-gray-200 bg-white" : "h-20 "}
       `}
     >
       <nav className="h-full max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
         <div className="flex items-center h-full">
-          {/* Logo */}
           <Link
             href="/"
-            className={`transition-all duration-300 ${
+            className={`transition-all duration-300 ease-in-out ${
               scrolled ? "scale-90" : "scale-100"
             }`}
           >
             <img
-              src="/logo.png"
+              src={`${scrolled ? "/logo.png" : "logo-blanco.png"}`}
               alt="Megatron"
-              className="h-10 w-auto hover:opacity-80 transition-opacity logo"
+              className={`w-auto hover:opacity-80 transition-opacity logo ${
+                scrolled ? "logo" : "logo2"
+              }`}
             />
           </Link>
 
-          {/* Línea Vertical de Separación */}
           <div className="border-l-2 border-gray-500 h-0 mx-4"></div>
 
-          {/* Menú de Navegación */}
           <div className="flex h-full">
             {["/", "/servicios", "/contacto", "/productos"].map(
               (route, index) => {
@@ -62,23 +65,33 @@ const Header = () => {
                   "CONTACTO",
                   "PRODUCTOS",
                 ];
+                const isActive = isRouteActive(route);
+
                 return (
                   <Link
                     key={route}
                     href={route}
                     className={`
-                    relative flex items-center h-full px-5
-                    ${isRouteActive(route) ? "text-blue-600" : "text-gray-700"}
-                    hover:bg-blue-50 transition-colors duration-200
-                  `}
+            relative flex items-center h-full px-5
+            ${scrolled ? "hover:bg-blue-50" : "hover:bg-blue-600"}
+            ${
+              isActive && scrolled
+                ? "text-blue-700"
+                : scrolled
+                ? "text-gray-700"
+                : "text-white"
+            }
+            transition-colors duration-200
+          `}
                   >
                     <span className="text-xs font-bold">
                       {routeNames[index]}
                     </span>
-                    {isRouteActive(route) && (
+                    {isActive && (
                       <div
-                        className="absolute left-0 w-full h-1 bg-blue-600"
-                        style={{ bottom: scrolled ? "-1px" : "0" }}
+                        className={`absolute left-0 w-full h-1 barrita ${
+                          scrolled ? "bg-blue-700" : "bg-white"
+                        }`}
                       ></div>
                     )}
                   </Link>
@@ -87,19 +100,22 @@ const Header = () => {
             )}
           </div>
 
-          {/* Espaciador flexible */}
           <div className="flex-grow"></div>
 
-          {/* Botón de Teléfono */}
           <button
-            className="
-              bg-blue-700 text-white px-6 py-2 rounded-full
-              hover:bg-blue-700 transform hover:scale-105
+            className={`
+              px-6 py-2 rounded-full
+              transform hover:scale-105
               transition-all duration-300 ease-in-out
               shadow-md hover:shadow-lg
               flex items-center space-x-2
               text-sm font-bold
-            "
+              ${
+                scrolled
+                  ? "bg-blue-700 text-white hover:bg-blue-800"
+                  : "bg-white text-blue-700 hover:bg-blue-100"
+              }
+            `}
           >
             <PhoneIcon className="w-4 h-4" />
             <span>Tel: 618-825-3884</span>
